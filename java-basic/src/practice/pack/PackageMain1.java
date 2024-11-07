a1d9550f50ff0232462c2ea4470e0eb0dbb91c3e
cchhaannn <ychan3397@naver.com>
2024-11-07 21:23:48 +0900

    👏 11월 7일

---
+++ java-basic/src/practice/pack/PackageMain1.java
@@ -0,0 +1,9 @@
+package practice.pack;
+
+public class PackageMain1 {
+
+    public static void main(String[] args) {
+        Data data = new Data();
+        practice.pack.a.User user = new practice.pack.a.User();
+    }
+}
